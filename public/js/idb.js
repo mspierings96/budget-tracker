94565f5325b8d61c5ee1925100e02fbf8fdc69e6
Your Name <you@example.com>
2021-07-22 20:17:25 -0500

    test push

---
+++ public/js/idb.js
@@ -0,0 +1,74 @@
+let db;
+const request = indexedDB.open("transaction", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("transaction", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run checkDatabase() function to send all local db data to api
+  if (navigator.onLine) {
+    uploadPizza();
+  }
+};
+
+request.onerror = function (event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["transaction"], "readwrite");
+
+  const pizzaObjectStore = transaction.objectStore("transaction");
+
+  // add record to your store with add method.
+  pizzaObjectStore.add(record);
+}
+
+function uploadPizza() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["transaction"], "readwrite");
+
+  // access your pending object store
+  const pizzaObjectStore = transaction.objectStore("transaction");
+
+  // get all records from store and set to a variable
+  const getAll = pizzaObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(["transaction"], "readwrite");
+          const pizzaObjectStore = transaction.objectStore("transaction");
+          // clear all items in your store
+          pizzaObjectStore.clear();
+        })
+        .catch((err) => {
+          // set reference to redirect back here
+          console.log(err);
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", uploadPizza);
